eaeeaf01a0b10b25edafbd92397dd72d2c623f19
huuuuubi <kuba@hubkiewicz.pl>
2021-11-10 14:48:02 +0100

    10.11

---
+++ ViKard/app/src/main/java/com/example/vikard/MainScreen.java
@@ -0,0 +1,29 @@
+package com.example.vikard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+//import com.example.vikard.databinding.ActivityMainScreenBinding;
+//import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        TabController tabcontroller = new TabController(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tabcontroller.addFragment(new Karty(),"Karty");
+        tabcontroller.addFragment(new Mapa(),"Mapa");
+        viewPager.setAdapter(tabcontroller);
+    }
+}
--- ViKard/app/src/main/java/com/example/vikard/ui/login/LoginActivity.java
+++ ViKard/app/src/main/java/com/example/vikard/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.vikard.MainScreen;
 import com.example.vikard.R;
 import com.example.vikard.ui.login.LoginViewModel;
 import com.example.vikard.ui.login.LoginViewModelFactory;
@@ -125,12 +127,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main_screen);
+        switchActivities();
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+
+    private void switchActivities() {
+        Intent switchActivityIntent = new Intent(this, MainScreen.class);
+        startActivity(switchActivityIntent);
     }
 }
