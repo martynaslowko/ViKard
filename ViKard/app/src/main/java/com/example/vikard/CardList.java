0e2b0bc2619906b0d4f3e58f42894279136ac488
martynaslowko <75419225+martynaslowko@users.noreply.github.com>
2021-12-05 16:47:36 +0100

    Card preview and full view, CardCollection for CardList
    - Refactoring of getCardDataSQL to unify method for both ShopsName and rest of CardModel variables.
    - Implemented two data getter types for preview mode (Card Collection) and full view (Card Element)
    - Provided data getter for User's Card Collection in CardList

--- ViKard/app/src/main/java/com/example/vikard/CardList.java
+++ ViKard/app/src/main/java/com/example/vikard/CardList.java
@@ -8,8 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vikard.data.SQLConnection;
+import com.example.vikard.data.model.CardModel;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class CardList extends Fragment {
+
+    ArrayList<CardModel> cardCollection = new ArrayList<CardModel>();
+
     public CardList() {
+        //populateCardCollection(1); //populates cardCollection object with Jane Doe's cards (Id = 1)
         // Required empty public constructor
     }
 
@@ -19,4 +31,20 @@ public class CardList extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_card_list, container, false);
     }
+
+    public void populateCardCollection (int UsersId){
+        SQLConnection sql = new SQLConnection();
+        Connection conn = sql.getConnection();
+        try {
+           Statement statement = conn.createStatement();
+           ResultSet resultSet = statement.executeQuery("SELECT Id FROM Cards WHERE UsersId="+UsersId);
+           while (resultSet.next()){
+               cardCollection.add(new CardModel(resultSet.getInt("Id"), false));
+           }
+        } catch (Exception ex) {
+        } finally {
+            try { conn.close(); } catch (Exception e) { }
+        }
+    }
+
 }
