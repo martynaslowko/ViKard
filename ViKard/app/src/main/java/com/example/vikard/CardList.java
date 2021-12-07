a13ab8f3960d71e0317493bd566f8c35c1e0c8b2
huuuuubi <kuba@hubkiewicz.pl>
2021-12-07 22:37:53 +0100

    nihil novi

--- ViKard/app/src/main/java/com/example/vikard/CardList.java
+++ ViKard/app/src/main/java/com/example/vikard/CardList.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.vikard.data.LoginRepository;
 import com.example.vikard.data.SQLConnection;
@@ -22,15 +23,24 @@ import java.util.ArrayList;
 public class CardList extends Fragment {
 
     ArrayList<CardModel> cardCollection = new ArrayList<CardModel>();
+    TextView tv1;
+    View rootView;
+    int user_id;
 
     public CardList() {
-        //populateCardCollection(Integer.valueOf(LoginRepository.user.getUserId()));
-        //populates cardCollection object with current logged-in user
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_card_list, container, false);
+        user_id = Integer.valueOf(LoginRepository.user.getUserId());
+        //populateCardCollection(Integer.valueOf(LoginRepository.user.getUserId()));
+        //populates cardCollection object with current logged-in user
+        populateCardCollection(user_id);
+
+        tv1 = (TextView) rootView.findViewById(R.id.output);
+        tv1.setText(String.valueOf(user_id));
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_card_list, container, false);
@@ -50,5 +60,4 @@ public class CardList extends Fragment {
             try { conn.close(); } catch (Exception e) { }
         }
     }
-
 }
