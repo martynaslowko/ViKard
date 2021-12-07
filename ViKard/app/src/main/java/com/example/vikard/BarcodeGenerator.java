037e93dbf2ef6f804686d834bafe35293ac16ebd
huuuuubi <kuba@hubkiewicz.pl>
2021-12-07 19:19:42 +0100

    Widok karty

---
+++ ViKard/app/src/main/java/com/example/vikard/BarcodeGenerator.java
@@ -0,0 +1,18 @@
+package com.example.vikard;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class BarcodeGenerator {
+}
