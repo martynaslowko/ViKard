5b0a2fbc237c58b207637a33c1847d26f7711a26
huuuuubi <kuba@hubkiewicz.pl>
2022-03-08 10:58:41 +0100

    karta

--- ViKard/app/src/main/java/com/example/vikard/Karta.java
+++ ViKard/app/src/main/java/com/example/vikard/Karta.java
@@ -1,27 +1,31 @@
 package com.example.vikard;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.graphics.drawable.DrawableCompat;
 
+import com.example.vikard.data.model.CardModel;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class Karta extends AppCompatActivity {
-    String shopName;
-    String expireDate;
-    String barcode;
-    String hexColor;
-    TextView shopName_;
-    TextView expireDate_;
-    TextView barcode_;
-    TextView barcode2_;
-    TextView barcodeText;
-
-
+    String shopName, expireDate, barcode, hexColor,link;
+    TextView shopName_, expireDate_, barcode_, barcodeText;
+    Boolean state;
+    ImageView boxBarcode;
+    Button linkbutton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Bundle b = getIntent().getExtras();
@@ -30,6 +34,7 @@ public class Karta extends AppCompatActivity {
             shopName = b.getString("shopName");
             expireDate = b.getString("expireDate");
             barcode = b.getString("barcode");
+            link = b.getString("link");
         }
         Drawable unwrappedDrawable = AppCompatResources.getDrawable(getBaseContext(), R.drawable.card);
         Drawable wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable);
@@ -42,11 +47,34 @@ public class Karta extends AppCompatActivity {
         expireDate_ = (TextView) findViewById(R.id.cardExpires);
         barcodeText = (TextView)findViewById(R.id.barcode_text);
         barcode_ = (TextView) findViewById(R.id.barcode);
-        barcode2_ = (TextView) findViewById(R.id.barcode2);
         shopName_.setText(shopName);
         expireDate_.setText(expireDate);
         barcode_.setText(barcode);
-        barcode2_.setText(barcode);
         barcodeText.setText(barcode);
+        boxBarcode = findViewById(R.id.imageView2);
+        linkbutton = findViewById(R.id.linkbutton);
+        barcode_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expireDate_.setVisibility(View.INVISIBLE);
+                barcode_.setVisibility(View.INVISIBLE);
+                barcodeText.setVisibility(View.INVISIBLE);
+                boxBarcode.setVisibility(View.INVISIBLE);
+                linkbutton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        linkbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToUrl(link);
+            }
+        });
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
