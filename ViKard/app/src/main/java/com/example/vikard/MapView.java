c6f3dfb34cf9004b7643a9a004699c05b5987036
Piotr Wojtkun <wp46696@zut.edu.pl>
2022-05-17 10:57:56 +0200

    Map WIP

--- ViKard/app/src/main/java/com/example/vikard/MapView.java
+++ ViKard/app/src/main/java/com/example/vikard/MapView.java
@@ -1,13 +1,5 @@
 package com.example.vikard;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
-import android.os.Bundle;
-
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,11 +8,20 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vikard.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -28,109 +29,174 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.StreetViewPanoramaOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
-public class MapView extends Fragment implements OnMapReadyCallback{
+public class MapView extends Fragment implements OnMapReadyCallback {
 
+    private static final String TAG = "";
+    private static final int REQUEST_LOCATION_PERMISSION = 1234 ;
     private GoogleMap mMap;
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap)
-    {
-        mMap =  googleMap;
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
         initMap();
+        enableMyLocation(mMap);
+        setMapStyle(mMap);
+
     }
 
-    //Tymczasowo zakomentowany, próbuje dojść do tego jak prosić użytkownika o zezwolenie na pobieranie lokalizacji
 
-
-
-//    private FusedLocationProviderClient mFusedLocationProviderClient;
-//    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-//    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-//    private Boolean mLocationPermissionGranted = false;
-//
-//    private void moveCamera(LatLng latLng, float zoom)
-//    {
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
-//    }
-//
-//    private void getLocationPermission()
-//    {
-//        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-//
-//        if(ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-//        {
-//            if(ContextCompat.checkSelfPermission(this.getActivity().getApplicationContext(),COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-//            {
-//                mLocationPermissionGranted = true;
-//            }
-//            else
-//            {
-//                ActivityCompat.requestPermissions(this.getActivity(), permissions,1234);
-//            }
-//        }
-//    }
-//
-//    private void getDeviceLocation() {
-//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
-//        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//
-//            Task location = mFusedLocationProviderClient.getLastLocation();
-//            location.addOnCompleteListener(new OnCompleteListener() {
-//                @Override
-//                public void onComplete(@NonNull Task task) {
-//                    Location currentLocation = (Location)task.getResult();
-//                    initMap();
-//
-//                    moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),15f);
-//                }
-//            });
-//
-//
-//        }
-//    }
-
-
-    private void initMap()
-    {
+    private void initMap() {
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
         supportMapFragment.getMapAsync(MapView.this);
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
                 MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                       mMap.clear();
-                       mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                       mMap.addMarker(markerOptions);
+                markerOptions.position(latLng);
+                mMap.clear();
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                mMap.addMarker(markerOptions);
             }
         });
 
     }
 
 
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.map_options, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Change the map type based on the user's selection.
+        switch (item.getItemId()) {
+            case R.id.normal_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.satellite_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                return true;
+            case R.id.terrain_map:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map_view, container, false);
-
+        setHasOptionsMenu(true);
         return view;
     }
+
+    private void setPoiClick(final GoogleMap map) {
+        map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
+            @Override
+            public void onPoiClick(PointOfInterest poi) {
+                Marker poiMarker = map.addMarker(new MarkerOptions()
+                        .position(poi.latLng)
+                        .title(poi.name));
+                poiMarker.showInfoWindow();
+                poiMarker.setTag(getString(R.string.poi));
+            }
+        });
+    }
+
+    /**
+     * Loads a style from the map_style.json file to style the Google Map. Log
+     * the errors if the loading fails.
+     *
+     * @param map The GoogleMap object to style.
+     */
+    private void setMapStyle(GoogleMap map) {
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.map_style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+    }
+
+    /**
+     * Checks for location permissions, and requests them if they are missing.
+     * Otherwise, enables the location layer.
+     */
+    private void enableMyLocation(GoogleMap map) {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    /**
+     * Starts a Street View panorama when an info window containing the poi tag
+     * is clicked.
+     *
+     * @param map The GoogleMap to set the listener to.
+     */
+    private void setInfoWindowClickToPanorama(GoogleMap map) {
+        map.setOnInfoWindowClickListener(
+                new GoogleMap.OnInfoWindowClickListener() {
+                    @Override
+                    public void onInfoWindowClick(Marker marker) {
+                        // Check the tag
+                        if (marker.getTag() == "poi") {
+
+                            // Set the position to the position of the marker
+                            StreetViewPanoramaOptions options =
+                                    new StreetViewPanoramaOptions().position(
+                                            marker.getPosition());
+
+                            SupportStreetViewPanoramaFragment streetViewFragment
+                                    = SupportStreetViewPanoramaFragment
+                                    .newInstance(options);
+
+                            // Replace the fragment and add it to the backstack
+//                            getSupportFragmentManager().beginTransaction()
+//                                    .replace(R.id.fragment_container,
+//                                            streetViewFragment)
+//                                    .addToBackStack(null).commit();
+                        }
+                    }
+                });
+    }
+
+
 
 
 }
